1362d8ea004a8bdfd78223e3d6c0ac0ec0eba2b6
DoctorMighty <helmstetternicholas5@gmail.com>
2020-02-19 14:01:35 -0500

    The three parts of the test we took.

---
+++ BarnPortion.js
@@ -0,0 +1,9 @@
+function barn (Color, sqrFt, anmlNum){
+    this.Color = Red,
+    this.sqrFt =  5250 Ft,
+    this.anmlNum = 50
+    
+    Paint : function (Color){
+      var color (userInput)
+     }
+    
